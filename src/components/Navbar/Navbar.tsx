0515159c93c549a9684e7fedb5c66a88d7b1069c
Rizwan Kabir <rizwankabirsizan@gmail.com>
2022-04-14 23:01:34 +0200

    ADD: Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Badge } from "@mui/material";
+
+const Container = styled.div`
+  height: 80px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgrey;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuList = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon style={{ color: "grey", fontSize: "16px" }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>Shoppers Club</Logo>
+        </Center>
+        <Right>
+          <MenuList>Register</MenuList>
+          <MenuList>Login</MenuList>
+          <MenuList>
+            <Badge badgeContent={2} color="warning">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuList>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
